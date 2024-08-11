cd82b4bcf010aba77889be029b120ed5f63d5086
Simón A. Azócar <saimn.azocar@gmail.com>
2024-08-11 19:44:02 -0400

    feat(api): ✨ add Pokemon entity and shema

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // id: string -> Mongo already creates it as MongoID
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
