812f68a20250f83ad221c54c3e871a6e0277550d
Simón A. Azócar <saimn.azocar@gmail.com>
2024-08-12 19:43:33 -0400

    feat(api): ✨ update pokemon works!

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -27,12 +27,7 @@ export class PokemonService {
       const pokemon = await this.pokemonModel.create(createPokemonDto);
       return pokemon;
     } catch (error) {
-      if (error.code === 11000)
-        throw new BadRequestException(
-          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
-        );
-      console.log(error);
-      throw new InternalServerErrorException();
+      this.handleExceptions(error);
     }
   }
 
@@ -64,11 +59,30 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(id: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(id);
+
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+
+    try {
+      await pokemon.updateOne(updatePokemonDto);
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000)
+      throw new BadRequestException(
+        `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+      );
+    console.log(error);
+    throw new InternalServerErrorException();
   }
 }
