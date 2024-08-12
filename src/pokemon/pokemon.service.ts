2a8700dcde90dcb16201132c7768f54391405b7f
Simón A. Azócar <saimn.azocar@gmail.com>
2024-08-12 18:46:52 -0400

    feat(api): create and find pokemon works!

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,21 +1,67 @@
-import { Body, Injectable, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
+import { isValidObjectId, Model } from 'mongoose';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   @Post()
-  create(@Body() createPokemonDto: CreatePokemonDto) {
+  async create(@Body() createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000)
+        throw new BadRequestException(
+          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+        );
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(id: string) {
+    let pokemon: Pokemon;
+
+    if (!isNaN(+id)) {
+      pokemon = await this.pokemonModel.findOne({ no: id });
+    }
+
+    if (!pokemon && isValidObjectId(id)) {
+      pokemon = await this.pokemonModel.findById(id);
+    }
+
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({
+        name: id.toLocaleLowerCase(),
+      });
+    }
+
+    if (!pokemon)
+      throw new NotFoundException(
+        `Pokemon with id, name or no "${id}" not found`,
+      );
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
